85ec87701597382d5baca5b1cc48dcd725b5ef08
Francisco Fernandez Ruiz <ffr9@alu.ua.es>
2024-01-23 21:06:08 +0100

    conexion con tpv peticiones al api realizadas

--- src/main/java/tiendaropa/controller/CarritoController.java
+++ src/main/java/tiendaropa/controller/CarritoController.java
@@ -17,6 +17,8 @@ import org.springframework.ui.Model;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Set;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class CarritoController {
@@ -28,6 +30,11 @@ public class CarritoController {
     private ManagerUserSession managerUserSession;
     @Autowired
     UsuarioService usuarioService;
+
+    private final String API_URL = "https://ebisu.firstrow2.com/api/transactions";
+    private final String API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTF9.5J3FXZKYWPPs0Cqx9O6R7sEhGXYfmw8onjFqFJt864E";
+
+
 
     @GetMapping("/tiendaropa/carrito")
     public String verCarrito(Model model) {
@@ -86,6 +93,131 @@ public class CarritoController {
         return "redirect:/tiendaropa/catalogo";
     }
 
+    @PostMapping("/tiendaropa/compra/checkout")
+    public String procesarCheckout(Model model) {
+        try {
+            Long usuarioId = managerUserSession.usuarioLogeado();
+            UsuarioData user = usuarioService.findById(usuarioId);
+
+            List<Usuario> usuarios = usuarioService.listadoCompleto();
+            Usuario usuario = usuarioService.buscarUsuarioPorId(usuarios, usuarioId);
+
+            System.out.println(usuario);
+
+            // Obtener el carrito del usuario
+            Carrito carrito = carritoService.obtenerCarritoUsuario(usuario);
+
+            // Realizar la petición POST
+            String respuestaAPI = hacerPeticionPostALaAPI(); // Implementa este método
+
+            // Aquí puedes manipular la respuesta de la API según tus necesidades
+            System.out.println("Respuesta de la API: " + respuestaAPI);
+
+            // Eliminar todas las líneas del carrito
+            carritoService.eliminarTodasLasLineasCarrito(carrito);
+
+            // Agregar la respuesta de la API al modelo para mostrarla en la vista
+            model.addAttribute("respuestaAPI", respuestaAPI);
+            model.addAttribute("usuario", user);
+
+        } catch (Exception e) {
+            System.err.println("Error durante el procesamiento del checkout.");
+            e.printStackTrace();
+        }
+
+        // Mostrar la vista checkout con la respuesta de la API
+        return "checkout";
+    }
+
+    public String hacerPeticionPostALaAPI() {
+        try {
+            // Crear el objeto de datos en el formato JSON
+            String jsonBody = "{\n" +
+                    "  \"concept\": \"string\",\n" +
+                    "  \"amount\": 10,\n" +
+                    "  \"receipt_number\": \"string\",\n" +
+                    "  \"payment\": {\n" +
+                    "    \"type\": \"paypal\",\n" +
+                    "    \"values\": {\n" +
+                    "      \"paypal_user\": \"string\",\n" +
+                    "      \"credit_card_number\": \"string\",\n" +
+                    "      \"credit_card_expiration_month\": 12,\n" +
+                    "      \"credit_card_expiration_year\": 0,\n" +
+                    "      \"credit_card_csv\": 999\n" +
+                    "    }\n" +
+                    "  }\n" +
+                    "}";
+
+            // Crear la configuración de la petición HTTP
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setBearerAuth(API_TOKEN);
+            HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
+
+            // Crear la instancia de RestTemplate
+            RestTemplate restTemplate = new RestTemplate();
+
+            // Hacer la petición POST y obtener la respuesta
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(API_URL, requestEntity, String.class);
+
+            // Verificar si la petición fue exitosa (código de estado 2xx)
+            if (responseEntity.getStatusCode().is2xxSuccessful()) {
+                return responseEntity.getBody();
+            } else {
+                // Si la petición no fue exitosa, manejar el error según tus necesidades
+                System.err.println("Error en la petición POST a la API. Código de estado: " + responseEntity.getStatusCodeValue());
+                return "Error en la petición POST a la API.";
+            }
+
+        } catch (Exception e) {
+            // Manejar cualquier excepción que pueda ocurrir durante la petición
+            System.err.println("Error al hacer la petición POST a la API.");
+            e.printStackTrace();
+            return "Error al hacer la petición POST a la API.";
+        }
+    }
+
+    @GetMapping("/tiendaropa/compra/checkout")
+    public String hacerPeticionGetALaAPI(Model model) {
+        try {
+            // Crear la configuración de la petición HTTP
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + API_TOKEN);
+
+            // Crear la instancia de RestTemplate
+            RestTemplate restTemplate = new RestTemplate();
+
+            // Configurar las cabeceras
+            HttpEntity<?> requestEntity = new HttpEntity<>(headers);
+
+            // Hacer la petición GET y obtener la respuesta
+            ResponseEntity<String> responseEntity = restTemplate.exchange(API_URL, HttpMethod.GET, requestEntity, String.class);
+
+            // Verificar si la petición fue exitosa (código de estado 2xx)
+            if (responseEntity.getStatusCode().is2xxSuccessful()) {
+                String respuestaApi = responseEntity.getBody();
+
+                // Agregar la respuesta al modelo
+                model.addAttribute("respuestaAPI", respuestaApi);
+
+                // Devolver el nombre de la plantilla que deseas mostrar (checkout.html)
+                return "checkout";
+            } else {
+                // Si la petición no fue exitosa, manejar el error según tus necesidades
+                System.err.println("Error en la petición GET a la API. Código de estado: " + responseEntity.getStatusCodeValue());
+                return "Error en la petición GET a la API.";
+            }
+
+        } catch (Exception e) {
+            // Manejar cualquier excepción que pueda ocurrir durante la petición
+            System.err.println("Error al hacer la petición GET a la API.");
+            e.printStackTrace();
+            return "Error al hacer la petición GET a la API.";
+        }
+    }
+
+
+    /*
     @GetMapping("/tiendaropa/compra/checkout")
     public String checkout(Model model) {
         try {
@@ -112,6 +244,9 @@ public class CarritoController {
             e.printStackTrace();
         }
 
+        //en la pagina de checkout mostrar la respuesta del servidor
         return "checkout";
     }
+    */
+
 }
